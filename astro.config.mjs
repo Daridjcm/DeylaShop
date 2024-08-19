8b2fd6b2aa86d39f8f5da5bf5f8f473eca44e6ec
Daridjcm <daricorreamedina@gmail.com>
2024-08-19 12:19:56 -0500

    Add logos in format PNG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
-import { siSimpleicons } from 'simple-icons';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react({      experimentalReactChildren: true}), siSimpleicons()],
+  integrations: [tailwind(), react({      experimentalReactChildren: true})],
 });
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,14 @@ import './logoscroll.css'
 export function Footer() {
     return (
         <footer>
-            <div className='carousel-container overflow-hidden relative'>
-            <div className='carousel-track flex animate-scroll space-x-8'>
+            <div className='carousel-container relative m-5 mb-5 bg-[#080808]'>
+            <div className='carousel-track flex animate-scroll space-x-8 p-2'>
                 {logos.map((logo, index) => (
-                <img key={index} src={logo.src} alt={logo.alt} className='w-32 h-auto object-contain' />
+                <img key={index} src={logo.src} alt={logo.alt} className='w-28 h-auto object-contain' />
                 ))}
                 {/* Repetimos los logos para crear el efecto infinito */}
                 {logos.map((logo, index) => (
-                <img key={index + logos.length} src={logo.src} alt={logo.alt} className='w-32 h-auto object-contain' />
+                <img key={index + logos.length} src={logo.src} alt={logo.alt} className='w-28 h-auto object-contain' />
                 ))}
             </div>
             </div>
